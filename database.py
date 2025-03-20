ea4a31cdd55008d1eccfd3dbc3b68bb0b27d0b28
Ruben Soler <rubensolert@gmail.com>
2025-03-19 22:51:05 -0500

    Corrige conexión con PostgreSQL en Railway

--- database.py
+++ database.py
@@ -3,15 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 📌 Obtener la URL de la base de datos desde la variable de entorno
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("❌ ERROR: La variable de entorno DATABASE_URL no está configurada.")
 
-# 📌 Crear la conexión a la base de datos
 engine = create_engine(DATABASE_URL)
-
-# 📌 Configurar SQLAlchemy
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
